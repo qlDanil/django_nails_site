76d621a4a122622ded6e6543c9d34228837741d3
Danil <zaharenkodanil1@gmail.com>
2020-01-21 00:40:45 +0700

    fix

--- mainApp/views.py
+++ mainApp/views.py
@@ -6,7 +6,3 @@ def index(request):
     services = Services.objects.all()
     return render(request, 'mainApp/homePage.html', {"services": services})
 
-
-def contact(request):
-    return render(request, 'mainApp/basic.html',
-                  {'values': ['Если у вас остались вопросы, то задавайте их мне по телефону', '+7 (913) 185-98-76']})
--- works/admin.py
+++ works/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from works.models import Works
-from mainApp.models import Services
 
 admin.site.register(Works)
-admin.site.register(Services)
