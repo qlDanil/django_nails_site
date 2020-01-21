312c17d294e6316e4a0c1231fd2d3846efcae7ef
Danil <zaharenkodanil1@gmail.com>
2020-01-21 13:25:08 +0700

    fix

--- nailsSite/settings.py
+++ nailsSite/settings.py
@@ -60,6 +60,24 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'nailsSite.urls'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
