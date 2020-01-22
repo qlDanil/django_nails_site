53629ebe1f5ff540c3aef2e4b081c916166be33b
Danil <zaharenkodanil1@gmail.com>
2020-01-22 13:55:44 +0700

    fix

--- nailsSite/settings.py
+++ nailsSite/settings.py
@@ -3,8 +3,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-#MEDIA_DIR = os.path.join(BASE_DIR, 'media')
-#MEDIA_ROOT = MEDIA_DIR
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = MEDIA_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
