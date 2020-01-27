cf0460339e79cd76f64dcd2e312debe45ff5f9d3
Danil <zaharenkodanil1@gmail.com>
2020-01-27 14:19:10 +0700

    favicon

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import RedirectView
+{% load static %}
 
 urlpatterns = [
     path('', views.index, name='index'),
+	path('favicon.iso/', RedirectView.as_view(url="% static 'mainApp/icons/favicon.ico' %}", permanent=True)),
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -5,4 +5,3 @@ from mainApp.models import Services
 def index(request):
     services = Services.objects.all()
     return render(request, 'mainApp/homePage.html', {"services": services})
-
