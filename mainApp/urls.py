65f90a3039b00f39b1d5a08b9a1723c12c1f9674
Danil <zaharenkodanil1@gmail.com>
2020-01-27 14:24:11 +0700

    favicon

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
-from django.views.generic.base import RedirectView
-{% load static %}
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-	path('favicon.iso/', RedirectView.as_view(url="% static 'mainApp/icons/favicon.ico' %}", permanent=True)),
 ]
