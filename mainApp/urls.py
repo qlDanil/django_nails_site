0a6d9f5480c989f621e62d0b1fb145e68442ce6b
Danil <zaharenkodanil1@gmail.com>
2020-01-27 14:44:57 +0700

    favicon

--- mainApp/urls.py
+++ mainApp/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-	path('favicon.ico/', views.view_favicon, name='view_favicon'),
 ]
--- mainApp/views.py
+++ mainApp/views.py
@@ -5,6 +5,3 @@ from mainApp.models import Services
 def index(request):
     services = Services.objects.all()
     return render(request, 'mainApp/homePage.html', {"services": services})
-	
-def view_favicon(request):
-    return render(request, 'mainApp/favicon.ico')
